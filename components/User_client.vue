58b932f1ac361c432b6d81d8596ff092178545bb
Lunalex <alexandre.meyer@lunatech.fr>
2020-01-24 18:30:11 +0100

    very small typo change

--- components/User_client.vue
+++ components/User_client.vue
@@ -5,7 +5,7 @@
         <b-col align-self="center"><h2>Client</h2></b-col>
       </b-row>
       <b-row cols="3" align-h="around" align-v="start">
-        <b-col class="parameter-name">Client:</b-col>
+        <b-col class="parameter-name">Company:</b-col>
         <b-col class="parameter-value">Lunatech</b-col>
       </b-row>
       <b-row cols="3" align-h="around" align-v="start">
